ebbcf5f268b077c7a0ad758b287e2d3d3dca1471
Harsh Thakur <harshthakur9030@gmail.com>
2020-06-24 16:49:03 +0530

    Try..
    Signed-off-by: Harsh Thakur <harshthakur9030@gmail.com>

--- integration_tests/sanity/k8s_tests.go
+++ integration_tests/sanity/k8s_tests.go
@@ -1,20 +1,26 @@
-// /*
-// Copyright 2019 The OpenEBS Authors
+/*
+Copyright 2019 The OpenEBS Authors
 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-//     http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// */
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
-// package sanity
+package sanity
+
+import (
+	"strings"
+
+	"github.com/openebs/node-disk-manager/integration_tests/k8s"
+)
 
 // import (
 // 	. "github.com/onsi/ginkgo"
@@ -69,36 +75,36 @@
 // 	})
 // })
 
-// // WaitForPodToBeRunningEventually waits for 2 minutes for the given pod to be
-// // in running state
-// func WaitForPodToBeRunningEventually(podPrefix string) bool {
-// 	return Eventually(func() string {
-// 		c, _ := k8s.GetClientSet()
-// 		pods, err := c.ListPodStatus()
-// 		Expect(err).NotTo(HaveOccurred())
-// 		for pod, state := range pods {
-// 			if strings.Contains(pod, podPrefix) {
-// 				return state
-// 			}
-// 		}
-// 		return ""
-// 	}, 120, 5).Should(Equal(k8s.Running))
-// }
+// WaitForPodToBeRunningEventually waits for 2 minutes for the given pod to be
+// in running state
+func WaitForPodToBeRunningEventually(podPrefix string) bool {
+	return Eventually(func() string {
+		c, _ := k8s.GetClientSet()
+		pods, err := c.ListPodStatus()
+		Expect(err).NotTo(HaveOccurred())
+		for pod, state := range pods {
+			if strings.Contains(pod, podPrefix) {
+				return state
+			}
+		}
+		return ""
+	}, 120, 5).Should(Equal(k8s.Running))
+}
 
-// // WaitForPodToBeDeletedEventually waits for 2 minutes for the given pod to
-// // get deleted
-// func WaitForPodToBeDeletedEventually(podPrefix string) bool {
-// 	return Eventually(func() int {
-// 		c, _ := k8s.GetClientSet()
-// 		pods, err := c.ListPodStatus()
-// 		Expect(err).NotTo(HaveOccurred())
+// WaitForPodToBeDeletedEventually waits for 2 minutes for the given pod to
+// get deleted
+func WaitForPodToBeDeletedEventually(podPrefix string) bool {
+	return Eventually(func() int {
+		c, _ := k8s.GetClientSet()
+		pods, err := c.ListPodStatus()
+		Expect(err).NotTo(HaveOccurred())
 
-// 		noOfPods := 0
-// 		for pod := range pods {
-// 			if strings.Contains(pod, podPrefix) {
-// 				noOfPods++
-// 			}
-// 		}
-// 		return noOfPods
-// 	}, 120, 5).Should(BeZero())
-// }
+		noOfPods := 0
+		for pod := range pods {
+			if strings.Contains(pod, podPrefix) {
+				noOfPods++
+			}
+		}
+		return noOfPods
+	}, 120, 5).Should(BeZero())
+}
--- integration_tests/sanity/ndm_integration_test.go
+++ integration_tests/sanity/ndm_integration_test.go
@@ -1,80 +1,81 @@
-// /*
-// Copyright 2019 The OpenEBS Authors
+/*
+Copyright 2019 The OpenEBS Authors
 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-//     http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// */
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
-// package sanity
+package sanity
 
-// import (
-// 	. "github.com/onsi/ginkgo"
-// 	. "github.com/onsi/gomega"
-// 	"github.com/openebs/node-disk-manager/integration_tests/k8s"
-// 	"testing"
-// )
+import (
+	"testing"
 
-// func TestNDM(t *testing.T) {
-// 	RegisterFailHandler(Fail)
-// 	RunSpecs(t, "Integration Test Suite")
-// }
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/openebs/node-disk-manager/integration_tests/k8s"
+)
 
-// // Initialize the suite
-// var _ = BeforeSuite(func() {
-// 	// Create the client set
-// 	c, err := k8s.GetClientSet()
-// 	Expect(err).NotTo(HaveOccurred())
+func TestNDM(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Integration Test Suite")
+}
 
-// 	// Create service account and cluster roles required for NDM
-// 	err = c.CreateNDMServiceAccount()
-// 	Expect(err).NotTo(HaveOccurred())
+// Initialize the suite
+var _ = BeforeSuite(func() {
+	// Create the client set
+	c, err := k8s.GetClientSet()
+	Expect(err).NotTo(HaveOccurred())
 
-// 	err = c.CreateNDMClusterRole()
-// 	Expect(err).NotTo(HaveOccurred())
+	// Create service account and cluster roles required for NDM
+	err = c.CreateNDMServiceAccount()
+	Expect(err).NotTo(HaveOccurred())
 
-// 	err = c.CreateNDMClusterRoleBinding()
-// 	Expect(err).NotTo(HaveOccurred())
+	err = c.CreateNDMClusterRole()
+	Expect(err).NotTo(HaveOccurred())
 
-// 	err = c.CreateNDMConfigMap()
-// 	Expect(err).NotTo(HaveOccurred())
+	err = c.CreateNDMClusterRoleBinding()
+	Expect(err).NotTo(HaveOccurred())
 
-// 	err = c.CreateNDMOperatorDeployment()
-// 	Expect(err).NotTo(HaveOccurred())
+	err = c.CreateNDMConfigMap()
+	Expect(err).NotTo(HaveOccurred())
 
-// 	// wait for all changes to happen
-// 	k8s.WaitForStateChange()
+	err = c.CreateNDMOperatorDeployment()
+	Expect(err).NotTo(HaveOccurred())
 
-// })
+	// wait for all changes to happen
+	k8s.WaitForStateChange()
 
-// // clean up all resources by NDM
-// var _ = AfterSuite(func() {
-// 	c, err := k8s.GetClientSet()
-// 	Expect(err).NotTo(HaveOccurred())
+})
 
-// 	err = c.DeleteNDMClusterRoleBinding()
-// 	Expect(err).NotTo(HaveOccurred())
+// clean up all resources by NDM
+var _ = AfterSuite(func() {
+	c, err := k8s.GetClientSet()
+	Expect(err).NotTo(HaveOccurred())
 
-// 	err = c.DeleteNDMClusterRole()
-// 	Expect(err).NotTo(HaveOccurred())
+	err = c.DeleteNDMClusterRoleBinding()
+	Expect(err).NotTo(HaveOccurred())
 
-// 	err = c.DeleteNDMServiceAccount()
-// 	Expect(err).NotTo(HaveOccurred())
+	err = c.DeleteNDMClusterRole()
+	Expect(err).NotTo(HaveOccurred())
 
-// 	err = c.DeleteNDMCRDs()
-// 	Expect(err).NotTo(HaveOccurred())
+	err = c.DeleteNDMServiceAccount()
+	Expect(err).NotTo(HaveOccurred())
 
-// 	err = c.DeleteNDMConfigMap()
-// 	Expect(err).NotTo(HaveOccurred())
+	err = c.DeleteNDMCRDs()
+	Expect(err).NotTo(HaveOccurred())
 
-// 	err = c.DeleteNDMOperatorDeployment()
-// 	Expect(err).NotTo(HaveOccurred())
-// })
+	err = c.DeleteNDMConfigMap()
+	Expect(err).NotTo(HaveOccurred())
+
+	err = c.DeleteNDMOperatorDeployment()
+	Expect(err).NotTo(HaveOccurred())
+})
